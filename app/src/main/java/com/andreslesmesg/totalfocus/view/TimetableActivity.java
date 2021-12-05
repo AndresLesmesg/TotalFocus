0e147abe65b9efe835c85b1a535f152c68fa0e25
Andres Lesmes <jalg3009@gmail.com>
2021-12-05 00:39:34 -0500

    add view timetables layout

--- app/src/main/java/com/andreslesmesg/totalfocus/view/TimetableActivity.java
+++ app/src/main/java/com/andreslesmesg/totalfocus/view/TimetableActivity.java
@@ -1,6 +1,9 @@
 package com.andreslesmesg.totalfocus.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +12,21 @@ import com.andreslesmesg.totalfocus.R;
 
 public class TimetableActivity extends AppCompatActivity {
 
+    private Button btn_save_timetable;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
+
+        btn_save_timetable = findViewById(R.id.btn_save_timetable);
+
+        btn_save_timetable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TimetableActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
